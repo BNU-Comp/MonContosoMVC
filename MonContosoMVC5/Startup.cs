28d48fd0f97e7ceeb5cf0318706b5345f839ee4c
Derek Peacock <derek_peacock@yahoo.com>
2019-11-04 11:37:58 +0000

    Add project files.

---
+++ MonContosoMVC5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MonContosoMVC5.Startup))]
+namespace MonContosoMVC5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
